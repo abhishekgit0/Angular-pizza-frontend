795169bc9708debcebd2abfa1544df6f0ea4949e
abhishekgit01 <abhishekgitlab7@gmail.com>
2022-09-21 11:03:11 +0530

    first commit

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
+import { Router } from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  public avail:boolean = false;
+  public msg:string="";
+  private baseUri: string = "http://localhost:3000";
+  private headers = new HttpHeaders().set('Content-Type', 'application/json');
+  constructor(private http: HttpClient, private router: Router) { }
+
+
+  addPizza(body:any) {
+   
+    return this.http.post('http://127.0.0.1:3000/addtocart', body, {
+      observe: 'body',
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+  }
+
+
+  deletePizza(body:any) {
+    return this.http.post('http://127.0.0.1:3000/deletefromcart', body, {
+      observe: 'body',
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+  }
+
+
+ EmptyCheck() {
+    return this.http.get(this.baseUri + "/emptycheck", { headers: this.headers });
+  }
+
+  getOrderMail() {
+    console.log(this.headers);
+    var email = localStorage.getItem('userid');
+    console.log(email);
+    let para = new HttpParams().set('email', email)
+    return this.http.get(this.baseUri + "/ordermail/" + email, { params: para });
+  }
+
+  deletecart()
+  {
+    return this.http.post(this.baseUri + "/placeorder", { headers: this.headers });
+  }
+}
